46ba44bf9d40881bf6585639bad5d462e78e679b
Steven Livingston <learn2swimapp@gmail.com>
2022-11-27 14:56:32 -0600

    add more things to the quiz page

--- quiz.js
+++ quiz.js
@@ -1,4 +1,8 @@
 const path = window.location.search.replace("?", "")
+function playAudio(audioPath) {
+  temp = new Audio(audioPath)
+  temp.play()
+}
 /*
 let length = Object.keys(loadedData.words).length;
       let num = Math.floor(Math.random() * length);
@@ -12,13 +16,23 @@ let length = Object.keys(loadedData.words).length;
 let seconds = 0;
 let minutes = 0;
 setInterval(() => {
-  if (seconds === 60) {
+  if (seconds === 59) {
     seconds = 0
     minutes++
   } else {
     seconds++
   }
-  time = (minutes < 10) ? "0" + minutes + ":" + seconds : minutes + ":" + seconds
+  let time;
+  if (minutes < 10) {
+    time = "0" + minutes + ":"
+  } else {
+    time = minutes + ":"
+  }
+  if (seconds < 10) {
+    time += ("0" + seconds)
+  } else {
+    time += seconds
+  }
   $(".time").text(time)
 }, 1000)
 const correctAudio = new Audio('/assets/audios/other/correct.mp3')
@@ -43,10 +57,10 @@ let streak = 0;
       //edit page
       $('.flag').attr("src", loadedData.flagPath)
       $('.tt').attr("title", answer)
-    new bootstrap.Tooltip($(".tt"))
+      new bootstrap.Tooltip($(".tt"))
       $(".title").text(loadedData.title);
       $(".tabTitle").text(loadedData.title);
-      $(".title").append(`<img src="${loadedData.titleIcon}" width="40px;" style="margin-top:-7px;">`)
+      $(".title").append(` <img src="${loadedData.titleIcon}" width="40px;" style="margin-top:-7px;">`)
       $(".question").append(loadedData.languageName)
       $(".flagEmoji").text(loadedData.flagEmoji)
       $("html").css("--formMainColor", loadedData.formMainColor)
@@ -127,12 +141,42 @@ let streak = 0;
             }
           }
         });
-
+        function loadPartOfSpeechEmoji(partOfSpeech) {
+          switch(partOfSpeech) {
+            case "noun":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/large-red-circle_1f534.png";
+              break;
+            case "adjective":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/large-orange-circle_1f7e0.png";
+              break;
+            case "verb":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/large-yellow-circle_1f7e1.png";
+              break;
+            case "adverb":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/large-green-circle_1f7e2.png";
+              break;
+            case "pronoun":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/large-blue-circle_1f535.png";
+              break;
+            case "number":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/large-purple-circle_1f7e3.png";
+              break;
+            case "article":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/large-brown-circle_1f7e4.png";
+              break;
+            case "preposition":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/black-circle_26ab.png";
+              break;
+            case "conjunction":
+              return "https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/white-circle_26aa.png";
+              break;
+          }
+        }
       //Generate flashcards
       $(document).ready(() => {
         Object.entries(loadedData.words).forEach((pair) => {
           $("#hidden").append(
-            `<div class="card"><div class="card-header"><b>${pair[0]}</b> (<i>${loadedData.partsOfSpeech[pair[0]][1]}</i>)</div><div class="card-body">${pair[1]}</div></div>`
+            `<div class="card"><div class="card-header"><b>${pair[0]}</b> (<i>${loadedData.partsOfSpeech[pair[0]][1]}</i>) <img src="${loadPartOfSpeechEmoji(loadedData.partsOfSpeech[pair[0]][0])}" width="24px" style="margin-top:-4px"></div><div class="card-body">${pair[1]} <img src="https://emojipedia-us.s3.amazonaws.com/source/microsoft-teams/337/speaker-high-volume_1f50a.png" width="24px" onclick="playAudio('/assets/audios/${loadedData.audioRoot}/${pair[0]}.mp3')"></div></div>`
           );
         });
         $(".divider").append("test");
