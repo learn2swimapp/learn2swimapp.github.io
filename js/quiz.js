1f8a8aa3befc3b6f87726a1dc8f1ea3c4035ce96
Steven Livingston <learn2swimapp@gmail.com>
2022-12-19 22:45:28 -0600

    aesthetic changes

--- js/quiz.js
+++ js/quiz.js
@@ -223,3 +223,11 @@ function regenerate() {
   $(".form__field").val("");
   $(".form__field").focus();
 }
+const parallax = document.getElementById("parallax");
+
+// Parallax Effect for DIV 1
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  // DIV 1 background will move slower than other elements on scroll.
+});
